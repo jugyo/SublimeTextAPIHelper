c48b46bea51c8019f3e5fa8c6ce2fa6bc05ff1a3
Kazuyuki Kohno <kkohno@twitter.com>
2013-10-08 22:34:00 +0900

    improve the output command

--- sublime_text_api_helper.py
+++ sublime_text_api_helper.py
@@ -18,11 +18,16 @@ class SublimeTextApiHelperCommand(sublime_plugin.TextCommand):
         def on_done(index):
             if index >= 0:
                 self.view.run_command("sublime_text_api_helper_output", {"text": functions[index][1]})
-                for s in self.view.sel():
-                    self.view.insert(edit, s.a, function_name)
         self.view.window().show_quick_panel(items, on_done)
 
 class SublimeTextApiHelperOutputCommand(sublime_plugin.TextCommand):
     def run(self, edit, text):
         for s in self.view.sel():
             self.view.replace(edit, s, text)
+
+        sel = list(self.view.sel())
+        self.view.sel().clear()
+        for s in sel:
+            end_of_selection = max(s.a, s.b)
+            print(end_of_selection)
+            self.view.sel().add(sublime.Region(end_of_selection, end_of_selection))
